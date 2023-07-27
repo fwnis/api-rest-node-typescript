7e318cf77445d27a66e15c3411550bade70138d8
Eduardo Fanis <eduardo.fanis@hotmail.com>
2023-07-27 17:35:08 -0300

    yup validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,13 +1,32 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface ICidade {
   nome: string;
 }
 
-export function create(req: Request<unknown, unknown, ICidade>, res: Response) {
-  const data = req.body;
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
-  console.log(data);
+export async function create(
+  req: Request<unknown, unknown, ICidade>,
+  res: Response
+) {
+  let validateData: ICidade | undefined = undefined;
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      },
+    });
+  }
+  console.log(validateData);
 
   return res.send("Create");
 }
